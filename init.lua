bd7a2490e98048b630c69cf694fd3d01e7848d15
fheldtm <fheldtm@gmail.com-fheldtm>
2024-02-14 00:20:15 +0900

    nvim config

--- init.lua
+++ init.lua
@@ -1,8 +1,2 @@
-local utils = require('utils')
-
-require('config')
-
--- we don't use plugins as root
-if not utils.is_root() then
-  require('plugins')
-end
+require("cjlee.core")
+require("cjlee.lazy")
