081d895ee56e618ec100880408cbad52fe95cba9
fheldtm <fheldtm@gmail.com-fheldtm>
2024-03-06 00:58:15 +0900

    2024-03-06 00:58

--- lua/cjlee/lazy.lua
+++ lua/cjlee/lazy.lua
@@ -11,4 +11,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("cjlee.plugins")
+require("lazy").setup({
+  { import = "cjlee.plugins" },
+  { import = "cjlee.plugins.lsp" }
+}, {
+  change_detection = {
+    notify = false,
+  },
+})
