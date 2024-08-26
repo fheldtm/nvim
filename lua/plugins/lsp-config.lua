012efa0240373fe67a51ef16da8d24087a3d5c30
fheldtm <fheldtm@gmail.com>
2024-08-26 18:30:29 +0900

    update neovim configs
    - add gitsigns
    - update lsp-config
      - Add vue-language-server to tsserver
      - Remove vetur-vls and new lsp volar for vue3

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,20 +8,35 @@ return {
       settings = {
         Lua = {
           diagnostics = {
-            globals = { "vim" },  -- Neovim API에서 사용되는 "vim"을 전역 변수로 인식하도록 설정
+            globals = { "vim" }, -- Neovim API에서 사용되는 "vim"을 전역 변수로 인식하도록 설정
           },
           workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),  -- Neovim의 런타임 파일을 LSP 서버에 라이브러리로 제공
+            library = vim.api.nvim_get_runtime_file("", true), -- Neovim의 런타임 파일을 LSP 서버에 라이브러리로 제공
           },
           telemetry = {
-            enable = false,  -- Telemetry 비활성화
+            enable = false, -- Telemetry 비활성화
           },
         },
       },
     })
 
+    -- vue_language_server mason registry path
+    local vue_language_server_path = require('mason-registry')
+        .get_package("vue-language-server")
+        :get_install_path() .. "/node_modules/@vue/language-server"
+
     -- JavaScript/TypeScript 언어 서버 설정
     lspconfig.tsserver.setup({
+      init_options = {
+        plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vue_language_server_path,
+            languages = { "vue" },
+          },
+        },
+      },
+      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
       settings = {
         -- JavaScript/TypeScript 설정 (필요에 따라 추가적인 설정 가능)
       },
@@ -42,11 +57,30 @@ return {
       -- HTML LSP 관련 추가 설정이 필요한 경우 여기에 추가
     })
 
-    -- Vue Language Server 설정
-    lspconfig.vuels.setup({
-      -- Vue LSP 관련 추가 설정이 필요한 경우 여기에 추가
-    })
+    local function find_typescript_sdk(root_dir)
+      -- 1. .pnpm 내부의 TypeScript 경로 찾기
+      local tsdk_pnpm = vim.fn.glob(root_dir .. '/node_modules/.pnpm/typescript@*/node_modules/typescript/lib')
 
-    -- dart
+      -- 2. 일반 node_modules 내부의 TypeScript 경로 찾기
+      local tsdk_node_modules = root_dir .. '/node_modules/typescript/lib'
+
+      -- 3. TypeScript 경로가 존재하면 반환, 없으면 nil 반환
+      if vim.fn.isdirectory(tsdk_node_modules) == 1 then
+        return tsdk_node_modules
+      elseif vim.fn.isdirectory(tsdk_pnpm) == 1 then
+        return tsdk_pnpm
+      else
+        return nil
+      end
+    end
+    lspconfig.volar.setup({
+      on_new_config = function(new_config, new_root_dir)
+        -- TypeScript SDK 경로 지정
+        local tsdk_path = find_typescript_sdk(new_root_dir)
+        if tsdk_path then
+          new_config.init_options.typescript.tsdk = tsdk_path
+        end
+      end
+    })
   end
 }
