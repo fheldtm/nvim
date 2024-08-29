3938c3578e9f470af84125814ccf054eafdd0499
fheldtm <fheldtm@github.com>
2024-08-29 18:20:53 +0900

    update neovim config

--- lua/cjlee/core/keymaps.lua
+++ lua/cjlee/core/keymaps.lua
@@ -7,6 +7,8 @@ vim.keymap.set("n", "S", "<Nop>")
 -- set keymap
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
 
 vim.keymap.set("n", "sh", "<C-w>h")
 vim.keymap.set("n", "sj", "<C-w>j")
