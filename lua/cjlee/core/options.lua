9fd3de409fe824cd5dd8d2048dc521ad9c7d1ca1
fheldtm <fheldtm@gmail.com-fheldtm>
2024-09-02 21:33:04 +0900

    fix: default shell pwsh when windows

--- lua/cjlee/core/options.lua
+++ lua/cjlee/core/options.lua
@@ -50,10 +50,13 @@ opt.swapfile = false
 -- })
 
 -- shell
-vim.o.shell = "pwsh"
-vim.o.shellcmdflag =
-"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
+local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
+if is_windows then
+  vim.o.shell = "pwsh"
+  vim.o.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
+end
