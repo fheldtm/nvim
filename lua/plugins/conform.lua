edcf145489a47a36fa94e1ae6a83c14a04625597
fheldtm <fheldtm@gmail.com-fheldtm>
2024-09-02 21:33:34 +0900

    fix: conform option change. lua, python remove

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,9 @@
 return {
   "stevearc/conform.nvim",
-  -- event = { "BufReadPre", "BufNewFile" },
+  -- event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    require('conform').setup({
+    require("conform").setup({
       formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black" },
         javascript = { "prettier" },
         vue = { "prettier" },
       },
