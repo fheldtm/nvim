bb22b4b66759f62f052cb2656d1d869adf10d7a3
fheldtm <fheldtm@github.com>
2024-09-09 18:12:59 +0900

    integrate luasnip and cmp. add cmp_luasnip plugin.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,32 +1,55 @@
 return {
-  -- nvim-cmp 플러그인
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",  -- LSP 자동 완성 소스
-    "neovim/nvim-lspconfig", -- LSP 서버 설정
-    "L3MON4D3/LuaSnip"       -- 스니펫 플러그인 (선택사항)
-  },
-  config = function()
-    local cmp = require('cmp')
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    config = function()
+      -- lua snip keymaps
+      require('plugins.snippets.keymap')
 
-    -- nvim-cmp 설정
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)  -- LuaSnip을 사용하는 경우
-        end,
-      },
-      sources = {
-        { name = 'nvim_lsp' },  -- LSP 자동 완성 소스
-        { name = 'path' },      -- 파일 경로 자동 완성 소스
-        { name = 'buffer' },    -- 버퍼에서 자동 완성 항목 가져오기
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-n>'] = cmp.mapping.select_next_item(), -- 자동 완성 항목 다음으로 이동
-        ['<C-p>'] = cmp.mapping.select_prev_item(), -- 자동 완성 항목 이전으로 이동
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- 항목 선택
-        ['<C-Space>'] = cmp.mapping.complete(),
-      }),
-    })
-  end
+      -- lua snippets
+      require("plugins.snippets.lua")
+
+      -- javascript, typescript snippets
+      require("plugins.snippets.common-js")
+    end
+  },
+  {
+    -- nvim-cmp 플러그인
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",  -- LSP 자동 완성 소스
+      "neovim/nvim-lspconfig", -- LSP 서버 설정
+      "L3MON4D3/LuaSnip"       -- 스니펫 플러그인 (선택사항)
+    },
+    config = function()
+      local cmp = require('cmp')
+
+      -- nvim-cmp 설정
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- LuaSnip을 사용하는 경우
+          end,
+        },
+        sources = {
+          { name = 'nvim_lsp' }, -- LSP 자동 완성 소스
+          { name = 'path' },     -- 파일 경로 자동 완성 소스
+          { name = 'buffer' },   -- 버퍼에서 자동 완성 항목 가져오기
+          { name = 'luasnip' },  -- LuaSnip 소스 추가
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-n>'] = cmp.mapping.select_next_item(),        -- 자동 완성 항목 다음으로 이동
+          ['<C-p>'] = cmp.mapping.select_prev_item(),        -- 자동 완성 항목 이전으로 이동
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- 항목 선택
+          ['<C-Space>'] = cmp.mapping.complete(),
+        }),
+      })
+    end
+  },
+  {
+    "saadparwaiz1/cmp_luasnip"
+  }
 }
