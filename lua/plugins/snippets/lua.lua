cbdd7c90c11bd30847c17a1ada0ba2ee3859017c
fheldtm <fheldtm@github.com>
2024-09-09 18:11:48 +0900

    remove lua snippets

--- lua/plugins/snippets/lua.lua
+++ lua/plugins/snippets/lua.lua
@@ -6,8 +6,4 @@ local i = ls.insert_node
 
 -- Lua 파일을 위한 스니펫 정의
 ls.add_snippets('lua', {
-  -- 스니펫 'func'를 입력하면 자동으로 함수 템플릿이 생성됨
-  s('func', {
-    t('function '), i(1, 'name'), t({ '(', ')', '', '\t' }), i(0), t({ '', 'end' }),
-  }),
 })
